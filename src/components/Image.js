61b82d9ae2c5a02b0f0fcb3fc90af4690f99ac3a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-03 18:03:21 -0500

    Render icons over the pictures when they get hovered

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,28 @@
+import { hover } from '@testing-library/user-event/dist/hover'
 import React from 'react'
 
 function Image ({className, img}) {
+
+    const [hovered, setHovered] = React.useState(false)
+    
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    
+
     return(
-        <div className={`${className} image-container`}>
+        <div className={`${className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} >
             <img src={img.url} className="image-grid" />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
 
 export default Image
+
+
+/**
+    <i className="ri-heart-line favorite"></i>
+
+    <i className="ri-add-circle-line cart"></i>
+     */

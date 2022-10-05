ba14db29118bea2d573dccd30283b09e05485f4a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-04 22:56:51 -0500

    Revert back to previous commit before customHook

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,34 +1,26 @@
-import React, {useState, useContext} from "react"
-import PropTypes from "prop-types"
+import React from "react";
 import {Context} from '../context/context'
-import useHover from "../hooks/useHover"
+import PropTypes from 'prop-types'
 
-function CartItem({item}) {
-    // const [hovered, setHovered] = useState(false)
-    const [hovered, ref] = useHover()
-    const {removeFromCart} = useContext(Context)
-    
-    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+function CartItem ({item}) {
+
+    const {removeFromCart} = React.useContext(Context)
+
+    const [hovered, setHovered] = React.useState(false)
     
     return (
         <div className="cart-item">
-            <i 
-                className={iconClassName} 
-                onClick={() => removeFromCart(item.id)}
-                ref={ref}
-            >
-            </i>
-            
+            <i className={hovered ? "ri-delete-bin-fill": "ri-delete-bin-line"} onClick={()=> removeFromCart(item.id)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
     )
-}
 
-CartItem.propTypes = {
-    item: PropTypes.shape({
-        url: PropTypes.string.isRequired
-    })
+    CartItem.propTypes = {
+        item: PropTypes.shape({
+            url: PropTypes.string.isRequired
+        })
+    }
 }
 
 export default CartItem
